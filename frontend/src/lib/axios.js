6550ab571438d8ad51043a26617393c709209681
KushKK29 <kush282930@gmail.com>
2025-03-28 18:56:44 +0530

    api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api"
-      : "https://my-chat-app-1-11jz.onrender.com/api",
+  baseURL: "https://my-chat-app-1-11jz.onrender.com/api",
   withCredentials: true,
 });
