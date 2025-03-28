801ba8c5873600738e471dc43da11298e2fc4d6f
KushKK29 <kush282930@gmail.com>
2025-03-28 18:53:26 +0530

    remove: credentials from cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,12 +16,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
